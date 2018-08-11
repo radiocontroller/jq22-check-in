1b3aa64f02c1f3226c2c4b8982ec145fa6bcfbc3
ubuntu <ubuntu@localhost.localdomain>
2018-08-11 12:26:33 +0800

    move dependencies phantomjs-2.1.1-linux-x86_64.tar.bz2 out

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker build -t jq22-check-in .
-docker run -it -d --name jq22-check-in jq22-check-in /bin/bash
+docker run -it -d -v /etc/localtime:/etc/localtime --name jq22-check-in jq22-check-in /bin/bash
 docker exec -it $(docker ps -aqf "name=jq22-check-in") service cron start
+docker rmi $(docker images -f "dangling=true" -q)
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,11 +1,7 @@
-#env :PATH, ENV['PATH']
 env :GEM_PATH, "/usr/local/bundle/"
 set :output, 'log/cron.log'
 set :bundle_command, "/usr/local/bundle/bin/bundle exec"
 
-require "active_support/all"
-Time.zone = 'Beijing'
-
-every 1.day, at: [Time.parse('9:30 am').utc, Time.parse('5:30 pm').utc] do
+every 1.day, at: ['9:30 am', '5:30 pm'] do
   rake "crawler:execute"
 end

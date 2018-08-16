3b7a8e1360125d12f0ac3b4ec0c240fa9b1b65b1
rcer <302643159@qq.com>
2018-08-16 12:33:23 +0800

    remove old container before restart

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker build -t jq22-check-in .
+docker rm -f $(docker ps -aqf "name=jq22-check-in")
 docker run -it -d -v /etc/localtime:/etc/localtime --name jq22-check-in jq22-check-in /bin/bash
 docker exec -it $(docker ps -aqf "name=jq22-check-in") service cron start
 docker rmi $(docker images -f "dangling=true" -q)
--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,6 @@ env :GEM_PATH, "/usr/local/bundle/"
 set :output, 'log/cron.log'
 set :bundle_command, "/usr/local/bundle/bin/bundle exec"
 
-every 1.day, at: ['9:30 am', '5:30 pm'] do
+every 1.day, at: ['09:30 am', '5:30 pm'] do
   rake "crawler:execute"
 end
